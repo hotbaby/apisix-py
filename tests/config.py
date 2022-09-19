b49404a14e3fbdbe11c53a446bb4c92191da5384
阳阳 <mengyangyang@speechocean.com>
2022-09-19 19:19:12 +0800

    Update test case

--- tests/config.py
+++ tests/config.py
@@ -2,7 +2,11 @@
 
 import os
 
-DOMAIN = 'http://apisix.ai-test.speechocean.com'
+DOMAIN = os.environ.get('APISIX_DOMAIN')
 USERNAME = 'admin'
 PASSWORD = os.environ.get('APISIX_PASSWORD')
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+print(f'domain: {DOMAIN} \n'
+      f'username: {USERNAME} \n'
+      f'password: {PASSWORD}')
